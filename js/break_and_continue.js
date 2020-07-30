3e474b04d15bcb56c1f1c74d452c989b6f2cb1fd
Edgar Gonzalez <gonzalez.edgar10@gmail.comm>
2020-07-30 16:34:47 -0500

    working on them

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,8 +1,15 @@
 
-var number = prompt("Choose an odd number between 1 and 50.")
-var numberToBreakAt = number
-    for (var x = 1; x <= 49; x += 2) {
-        if (number % 2 === 0) || (number === ""){
-            break
-        }
+while(true) {
+    var randomNumber = parseInt(prompt("Give me an odd number between 1 and 50."));
+    if (randomNumber % 2 !== 0 && randomNumber <= 50 && randomNumber >= 1) {
+        break;
     }
+}
+console.log ("Number to skip is: " + randomNumber);
+for (var i = 1; i <= 50; i+=2) {
+    if (i === randomNumber) {
+        console.log('Yikes! Skipping number:' + i);
+        continue;
+    }
+    console.log('Here is odd number: ' + i);
+}
--- js/for_loops.js
+++ js/for_loops.js
@@ -1,25 +1,13 @@
-function showMultiplicationTable(input){
-for (var x = 1; x < 11; x++){
-console.log((input + '*' + x + '=' + (input * x)))
-}
-}
-showMultiplicationTable(1);
-showMultiplicationTable(2);
-showMultiplicationTable(3);
-showMultiplicationTable(4);
-showMultiplicationTable(5);
-showMultiplicationTable(6);
-showMultiplicationTable(7);
-showMultiplicationTable(8);
-showMultiplicationTable(9);
-showMultiplicationTable(10);
-
-// Don't modify the following line, it generates a random number between 1 and 3
-// and stores it in a variable named random
+// function showMultiplicationTable(input){
+//     for (var x = 1; x <= 10; x++){
+//         console.log((input + ' x ' + x + " = " + (input * x) ))
+//         }
+//     }
+// showMultiplicationTable(7)
 
 
 // var random = Math.floor((Math.random() * 180) + 20);
-// for (var x = 0; x<= 10; x++) {
+// for (var x = 1; x<= 10; x++) {
 //     var random = Math.floor((Math.random() * 180) + 20);
 //     if (random % 2 === 0){
 //         console.log(random + " is and even number.");
@@ -28,18 +16,30 @@ showMultiplicationTable(10);
 //     }
 // }
 
-
-// for (var x = 1; x < 10; x++) {
-//     var tier = x.toString();
-//     for (var y = 1; y < x; y++);{
-//         tier += x;
-//     }
-//     console.log(tier);
+// function getRandomInt(min, max) {
+//     return Math.floor(Math.random() * (max - min +1) + min);
 // }
-
-// for (var x = 100; x > 0; x-=5){
-//         console.log(x)
+//
+// for(var x = 1; x <= 10; x++) {
+//     var randomNumber = getRandomInt(20, 200);
+//
+//     if (randomNumber % 2 === 0) {
+//         console.log(randomNumber + " is even");
+//     } else {
+//         console.log(randomNumber + " is odd");
 //     }
-
+// }
+//
+//
+// for (var z = 1; z <= 9; z++) {
+//     console.log(z.toString().repeat(z));
+// }
+//
+//
+//
+// for (var w = 100; w > 0; w-=5){
+//         console.log(w)
+//     }
+//
 
 
